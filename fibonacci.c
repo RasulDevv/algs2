547a60a415b1cb194fbca4d1bda6a428653a28eb
RasulDevv <rasuldevv@gmail.com>
2023-02-10 10:53:45 +0500

    I added fibonacci algs.

--- fibonacci.c
+++ fibonacci.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// k-tartibli fibonacci sonini topib beruvchi funksiya
-void fib1(int k){
-    int n1=0, n2=1, x;
-    for(int i=0; i<k-2; i++){
-        x=n2;
-        n2=n1+n2;
-        n1=x;
-    }
-    printf("The %dth fibonacci number: %d\n", k, n2);
+// siz kiritgan a son fibonacci sonimi yoki fibonacci soni emasmi??? 
+void fib1(int a){ // Shu savolga funksiya javob beradi.
+	int n1=0, n2=1, x, q=0;
+	for(int i=0; i<=n2; i++){
+		x=n2;
+		n2=n1+n2;
+		if(n2==a || n1==a){
+			q++;
+			break;
+		}
+		n1=x;
+	}
+	if(q==1) printf("True");
+	if(q==0) printf("False");
 }
 
-// siz kiritgan a son fibonacci sonimi yoki fibonacci soni emasmi??? 
-void fib2(int a){ // Shu savolga funksiya javob beradi.
-    int n1=0, n2=1, x, n3=0;
-    for(int i=0; i<=n2; i++){
-        x=n2;
-        n2=n1+n2;
-        n1=x;
-        if(n2==a){
-            n3=1;
-            break;
-        }
-    }
-    if(n3==1){
-        printf("%d is fibonacci number.", a);
-    }
-    else if(n3==0){
-        printf("%d is not fibonacci number.", a);
-    }
+void fib2(int k){ // k-tartibli fibonacci sonini topib beruvchi funksiya
+	int n1=0, n2=1, x, i=0, q;
+	if(k==1){
+		printf("The %dth fibonacci number is %d.", k, n1);
+	}
+	else if(k>1 && k!=1){
+		do{
+			x=n2;
+			n2=n1+n2;
+			n1=x;
+			i++;	
+		} while (i<k-2);
+		printf("The %dth fibonacci number is %d.", k, n2);
+	}
+	else if(k<1){
+		printf("You entered the wrong number");
+	}
 }
 
 void main(){
-    // fib1(10);
-    // fib2(10946);
+	// fib1((int)144);
+	// fib2((int)9);
 }
