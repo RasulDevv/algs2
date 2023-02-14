a39ae25f6655652472fff598c0848a266b9fb4a8
RasulDevv <rasuldevv@gmail.com>
2023-02-14 15:51:32 +0500

    interesting factorial

--- factorial.c
+++ factorial.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long int factorial(x){
+long int fact(int x){
+    int y=1, i=1;
+    if(x==0) return 1;
+    else if(x<0) return 0;
+    else{
+        start:
+        y = y*i;
+        i++;
+        if(i<=x) goto start;
+        return y;
+    }
+}
+
+long int factorial(int x){
     if(x<0) return 0;
     if(x==0) return 1;
     return x*factorial(x-1);
 }
 
 void main(){
-    printf("%ld", factorial(4));
+    printf("%ld", fact(5));
 }
